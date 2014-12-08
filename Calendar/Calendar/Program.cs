61aa0396cd44535801b98cd8c3add9b1396db8d6
Vitaly Ishkulov <vit@iva-soft.com>
2014-12-08 20:27:31 +0500

    Added IO and program structure

--- Calendar/Calendar/Program.cs
+++ Calendar/Calendar/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,38 @@ namespace Calendar
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Using: {0} <dd.mm.yyyy>", AppDomain.CurrentDomain.FriendlyName);
+				return;
+			}
+			try
+			{
+				var dateArr = args[0].Split('.').Select(x => int.Parse(x)).ToArray();
+				var date = new DateTime(dateArr[2], dateArr[1], 1);
+				var firstDay = (int)date.DayOfWeek - 1;
+				var days = DateTime.DaysInMonth(dateArr[2], dateArr[1]);
+				GregorianCalendar gc = new GregorianCalendar();
+				var weekNumber = gc.GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+
+				for (int i = 0; i < days; i++)
+				{
+					if (i == 0 || (i + firstDay) % 7 == 0)
+					{
+						// append week number
+						// increase week number
+					}
+					if (i == dateArr[0] - 1)
+					{
+						// append circle
+					}
+					// append day
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Program exception: {0}", e.Message);
+			}
 		}
 	}
 }
